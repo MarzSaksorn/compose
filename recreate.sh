483834c319f2af8de59c34268ee1eff7bc41a323
Marzsaksorn <mark94849@proton.me>
2026-02-25 12:58:05 +0700

    add recreate.sh

---
+++ recreate.sh
@@ -0,0 +1,1 @@
+docker compose -f compose.yaml -f modules/node-checkers_multiplayer.yaml  -f modules/node-marzweb.yaml -f modules/Gamehub.yaml down && docker compose -f compose.yaml -f modules/node-checkers_multiplayer.yaml -f modules/node-marzweb.yaml -f modules/Gamehub.yaml up
